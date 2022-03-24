d1c754a268d8f32960a36ead7547d06ab5daf9c6
PasindiWeerathunge <dmppw96@gmail.com>
2022-03-25 01:55:47 +0530

    Server Configuration

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
-console.log("Hello New Project");
+import express from "express";
+import cors from "cors";
+import 'dotenv/config'
+
+const app = express();
+const PORT = process.env.PORT || "8090";
+
+app.use(cors());
+app.use(express.json({limit:"50mb"}));
+
+app.get("/", (req, res, next)=>{
+    res.send("<h2>Pregnancy Management System<h2>");
+    next();
+})
+
+app.listen(PORT, ()=>{
+    console.log('Server is running on PORT '+ PORT);
+})
